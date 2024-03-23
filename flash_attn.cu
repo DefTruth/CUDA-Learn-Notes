b1d4017082d499e887077bb2b5747109e0fd7a8f
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-03-23 16:05:33 +0800

    Update flash_attn.cu

--- flash_attn.cu
+++ flash_attn.cu
@@ -3,10 +3,10 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-__global__
-void forward_kernel(const float* Q, const float* K, const float* V, const int N, const int d,
-                    const int Tc, const int Tr, const int Bc, const int Br, const float softmax_scale,
-                    float* l, float *m, float* O) {
+// This kernel source from: https://github.com/tspeterkim/flash-attention-minimal/blob/main/flash.cu  
+__global__ void forward_kernel(const float* Q, const float* K, const float* V, const int N, const int d,
+                               const int Tc, const int Tr, const int Bc, const int Br, const float softmax_scale,
+                               float* l, float *m, float* O) {
     int tx = threadIdx.x;
     int bx = blockIdx.x; int by = blockIdx.y;  // batch and head index
 
