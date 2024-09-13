2fb595a9526e0feffdfb822c3101406b35a001f2
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-09-13 12:17:32 +0800

    [LayerNorm][Half] support fp16x8 packed LayerNorm (#19)
    * Update README.md
    
    * Update block_all_reduce.cu
    
    * Update dot_product.cu
    
    * Update layer_norm.cu
    
    * Update layer_norm.py
    
    * Update README.md
    
    * Update README.md

--- layer-norm/layer_norm.cu
+++ layer-norm/layer_norm.cu
@@ -13,6 +13,7 @@
 #define WARP_SIZE 32
 #define INT4(value) (reinterpret_cast<int4*>(&(value))[0])
 #define FLOAT4(value) (reinterpret_cast<float4*>(&(value))[0])
+#define HALF2(value) (reinterpret_cast<half2*>(&(value))[0])
 
 // -------------------------------------- FP32 -------------------------------------- 
 // Warp Reduce Sum
@@ -63,7 +64,7 @@ __global__ void layer_norm_f32_kernel(float* x, float* y, float g, float b, int 
   __syncthreads();
   float variance = (value - s_mean) * (value - s_mean);
   variance = block_reduce_sum_f32<NUM_THREADS>(variance);
-  if (tid == 0) s_variance = rsqrtf(variance / (float) K + epsilon);
+  if (tid == 0) s_variance = rsqrtf(variance / ((float) K + epsilon));
   // wait for s_variance in shared memory to be ready for all threads
   __syncthreads();
   if (idx < N * K) y[idx] = ((value - s_mean) * s_variance) * g + b;
@@ -97,7 +98,7 @@ __global__ void layer_norm_f32x4_kernel(float* x, float* y, float g, float b, in
   float variance = reg_x_hat.x * reg_x_hat.x + reg_x_hat.y * reg_x_hat.y 
                  + reg_x_hat.z * reg_x_hat.z + reg_x_hat.w * reg_x_hat.w;
   variance = block_reduce_sum_f32<NUM_THREADS>(variance);
-  if (tid == 0) s_variance = rsqrtf(variance / (float) K + epsilon);
+  if (tid == 0) s_variance = rsqrtf(variance / ((float) K + epsilon));
   // wait for s_variance in shared memory to be ready for all threads
   __syncthreads();
   float4 reg_y;
@@ -106,6 +107,219 @@ __global__ void layer_norm_f32x4_kernel(float* x, float* y, float g, float b, in
   reg_y.z = reg_x_hat.z * s_variance * g + b;
   reg_y.w = reg_x_hat.w * s_variance * g + b;
   if (idx < N * K) FLOAT4(y[idx]) = reg_y;
+}
+
+// -------------------------------------- FP16 -------------------------------------- 
+// Warp Reduce Sum: Half
+template<const int kWarpSize = WARP_SIZE>
+__device__ __forceinline__ half warp_reduce_sum_f16_f16(half val) {
+  #pragma unroll
+  for (int mask = kWarpSize >> 1; mask >= 1; mask >>= 1) {
+    // val = __hadd(val, __shfl_xor_sync(0xffffffff, val, mask));
+    val += __shfl_xor_sync(0xffffffff, val, mask);
+  }
+  return val;
+}
+
+template<const int kWarpSize = WARP_SIZE>
+__device__ __forceinline__ float warp_reduce_sum_f16_f32(half val) {
+  float val_f32 = __half2float(val);
+  #pragma unroll
+  for (int mask = kWarpSize >> 1; mask >= 1; mask >>= 1) {
+    val_f32 += __shfl_xor_sync(0xffffffff, val_f32, mask);
+  }
+  return val_f32;
+}
+
+template<const int NUM_THREADS=256>
+__device__ half block_reduce_sum_f16_f16(half val) {
+  // always <= 32 warps per block (limited by 1024 threads per block)
+  constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE - 1) / WARP_SIZE;
+  int warp = threadIdx.x / WARP_SIZE;
+  int lane = threadIdx.x % WARP_SIZE;
+  static __shared__ half shared[NUM_WARPS];
+  // reduce using half dtype within warps
+  val = warp_reduce_sum_f16_f16<WARP_SIZE>(val);
+  if (lane == 0) shared[warp] = val;
+  __syncthreads();
+  val = (lane < NUM_WARPS) ? shared[lane] : __float2half(0.0f);
+  val = warp_reduce_sum_f16_f16<NUM_WARPS>(val);
+  return val; // half
+}
+
+template<const int NUM_THREADS=256>
+__device__ float block_reduce_sum_f16_f32(half val) {
+  // always <= 32 warps per block (limited by 1024 threads per block)
+  constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE - 1) / WARP_SIZE;
+  int warp = threadIdx.x / WARP_SIZE;
+  int lane = threadIdx.x % WARP_SIZE;
+  static __shared__ float shared[NUM_WARPS];
+  // reduce using float dtype within warps
+  float val_f32 = warp_reduce_sum_f16_f32<WARP_SIZE>(val);
+  if (lane == 0) shared[warp] = val_f32;
+  __syncthreads();
+  val_f32 = (lane < NUM_WARPS) ? shared[lane] : 0.0f;
+  val_f32 = warp_reduce_sum_f32<NUM_WARPS>(val_f32);
+  return val_f32; // float
+}
+
+template<const int NUM_THREADS=256>
+__global__ void layer_norm_f16_f16_kernel(half* x, half* y, float g, float b, int N, int K) {
+  int tid = threadIdx.x; // 0..K-1
+  int bid = blockIdx.x; // 0..N-1
+  int idx = bid * blockDim.x + threadIdx.x;
+  const half epsilon = __float2half(1e-5f);
+  const half g_      = __float2half(g);
+  const half b_      = __float2half(b);
+  const half K_      = __int2half_rn(K);
+
+  __shared__ half s_mean; // shared within block
+  __shared__ half s_variance; // shared within block
+  half value = (idx < N * K) ? x[idx] : __float2half(0.0f); // load once only
+  half sum = block_reduce_sum_f16_f16<NUM_THREADS>(value);
+  if (tid == 0) s_mean = sum / K_;
+  // wait for s_mean in shared memory to be ready for all threads
+  __syncthreads();
+  half variance = (value - s_mean) * (value - s_mean);
+  variance = block_reduce_sum_f16_f16<NUM_THREADS>(variance);
+  if (tid == 0) s_variance = hrsqrt(variance / (K_ + epsilon));
+  // wait for s_variance in shared memory to be ready for all threads
+  __syncthreads();
+  if (idx < N * K) { 
+    y[idx] = __hfma((value - s_mean) * s_variance, g_, b_);
+    // y[idx] = ((value - s_mean) * s_variance) * g_ + b_; 
+  }
+}
+
+template<const int NUM_THREADS=256>
+__global__ void layer_norm_f16x2_f16_kernel(half* x, half* y, float g, float b, int N, int K) {
+  int tid = threadIdx.x; // 0..K-1
+  int bid = blockIdx.x; // 0..N-1
+  int idx = (bid * blockDim.x + threadIdx.x) * 2;
+  const half epsilon = __float2half(1e-5f);
+  const half g_      = __float2half(g);
+  const half b_      = __float2half(b);
+  const half K_      = __int2half_rn(K);
+
+  __shared__ half s_mean; // shared within block
+  __shared__ half s_variance; // shared within block
+  half2 reg_x = HALF2(x[idx]);
+  half value = (idx < N * K) ? (reg_x.x + reg_x.y) : __float2half(0.0f);
+  half sum = block_reduce_sum_f16_f16<NUM_THREADS>(value);
+  if (tid == 0) s_mean = sum / K_;
+  // wait for s_mean in shared memory to be ready for all threads
+  __syncthreads();
+  half2 reg_x_hat;
+  reg_x_hat.x = reg_x.x - s_mean;
+  reg_x_hat.y = reg_x.y - s_mean;
+  half variance = reg_x_hat.x * reg_x_hat.x + reg_x_hat.y * reg_x_hat.y;
+  variance = block_reduce_sum_f16_f16<NUM_THREADS>(variance);
+  if (tid == 0) s_variance = hrsqrt(variance / (K_ + epsilon));
+  // wait for s_variance in shared memory to be ready for all threads
+  __syncthreads();
+  if (idx < N * K) { 
+    half2 reg_y;
+    reg_y.x = __hfma(reg_x_hat.x * s_variance, g_, b_);
+    reg_y.y = __hfma(reg_x_hat.y * s_variance, g_, b_);
+    HALF2(y[idx]) = reg_y;
+  }
+}
+
+template<const int NUM_THREADS=256>
+__global__ void layer_norm_f16x8_f16_kernel(half* x, half* y, float g, float b, int N, int K) {
+  int tid = threadIdx.x; // 0..K-1
+  int bid = blockIdx.x; // 0..N-1
+  int idx = (bid * blockDim.x + threadIdx.x) * 8;
+  const half epsilon = __float2half(1e-5f);
+  const half g_      = __float2half(g);
+  const half b_      = __float2half(b);
+  const half K_      = __int2half_rn(K);
+
+  __shared__ half s_mean; // shared within block
+  __shared__ half s_variance; // shared within block
+  // manual unroll, load 128 bits in 1 memory issue.
+  half2 reg_x_0 = HALF2(x[idx + 0]);
+  half2 reg_x_1 = HALF2(x[idx + 2]);
+  half2 reg_x_2 = HALF2(x[idx + 4]);
+  half2 reg_x_3 = HALF2(x[idx + 6]);
+  half value = (((idx + 0) < N * K) ? (reg_x_0.x + reg_x_0.y) 
+                                      : __float2half(0.0f));
+  value     += (((idx + 2) < N * K) ? (reg_x_1.x + reg_x_1.y) 
+                                      : __float2half(0.0f));
+  value     += (((idx + 4) < N * K) ? (reg_x_2.x + reg_x_2.y) 
+                                      : __float2half(0.0f));
+  value     += (((idx + 6) < N * K) ? (reg_x_3.x + reg_x_3.y) 
+                                      : __float2half(0.0f));
+  half sum = block_reduce_sum_f16_f16<NUM_THREADS>(value);
+  if (tid == 0) s_mean = sum / K_;
+  // wait for s_mean in shared memory to be ready for all threads
+  __syncthreads();
+  // manual unroll
+  half2 reg_x_hat_0, reg_x_hat_1, reg_x_hat_2, reg_x_hat_3;
+  reg_x_hat_0.x = reg_x_0.x - s_mean;
+  reg_x_hat_0.y = reg_x_0.y - s_mean;
+  reg_x_hat_1.x = reg_x_1.x - s_mean;
+  reg_x_hat_1.y = reg_x_1.y - s_mean;
+  reg_x_hat_2.x = reg_x_2.x - s_mean;
+  reg_x_hat_2.y = reg_x_2.y - s_mean;
+  reg_x_hat_3.x = reg_x_3.x - s_mean;
+  reg_x_hat_3.y = reg_x_3.y - s_mean;
+  half variance = (((idx + 0) < N * K) ? (reg_x_hat_0.x * reg_x_hat_0.x 
+                                        + reg_x_hat_0.y * reg_x_hat_0.y) 
+                                        : __float2half(0.0f));
+  variance     += (((idx + 2) < N * K) ? (reg_x_hat_1.x * reg_x_hat_1.x 
+                                        + reg_x_hat_1.y * reg_x_hat_1.y) 
+                                       : __float2half(0.0f));
+  variance     += (((idx + 4) < N * K) ? (reg_x_hat_2.x * reg_x_hat_2.x 
+                                        + reg_x_hat_2.y * reg_x_hat_2.y) 
+                                        : __float2half(0.0f));
+  variance     += (((idx + 6) < N * K) ? (reg_x_hat_3.x * reg_x_hat_3.x 
+                                        + reg_x_hat_3.y * reg_x_hat_3.y) 
+                                        : __float2half(0.0f));
+  variance = block_reduce_sum_f16_f16<NUM_THREADS>(variance);
+  if (tid == 0) s_variance = hrsqrt(variance / (K_ + epsilon));
+  // wait for s_variance in shared memory to be ready for all threads
+  __syncthreads();
+  // manual unroll
+  half2 reg_y_0, reg_y_1, reg_y_2, reg_y_3;
+  reg_y_0.x = __hfma(reg_x_hat_0.x * s_variance, g_, b_);
+  reg_y_0.y = __hfma(reg_x_hat_0.y * s_variance, g_, b_);
+  reg_y_1.x = __hfma(reg_x_hat_1.x * s_variance, g_, b_);
+  reg_y_1.y = __hfma(reg_x_hat_1.y * s_variance, g_, b_);
+  reg_y_2.x = __hfma(reg_x_hat_2.x * s_variance, g_, b_);
+  reg_y_2.y = __hfma(reg_x_hat_2.y * s_variance, g_, b_);
+  reg_y_3.x = __hfma(reg_x_hat_3.x * s_variance, g_, b_);
+  reg_y_3.y = __hfma(reg_x_hat_3.y * s_variance, g_, b_);
+  if ((idx + 0) < N * K) { HALF2(y[idx + 0]) = reg_y_0; }
+  if ((idx + 2) < N * K) { HALF2(y[idx + 2]) = reg_y_1; }
+  if ((idx + 4) < N * K) { HALF2(y[idx + 4]) = reg_y_2; }
+  if ((idx + 6) < N * K) { HALF2(y[idx + 6]) = reg_y_3; }
+}
+
+template<const int NUM_THREADS=256>
+__global__ void layer_norm_f16_f32_kernel(half* x, half* y, float g, float b, int N, int K) {
+  int tid = threadIdx.x; // 0..K-1
+  int bid = blockIdx.x; // 0..N-1
+  int idx = bid * blockDim.x + threadIdx.x;
+  const float epsilon = 1e-5f;
+
+  __shared__ float s_mean; // shared within block
+  __shared__ float s_variance; // shared within block
+  float value = (idx < N * K) ? __half2float(x[idx]) : 0.0f; // load once only
+  float sum = block_reduce_sum_f32<NUM_THREADS>(value);
+  if (tid == 0) s_mean = sum / (float) K;
+  // wait for s_mean in shared memory to be ready for all threads
+  __syncthreads();
+  float variance = (value - s_mean) * (value - s_mean);
+  variance = block_reduce_sum_f32<NUM_THREADS>(variance);
+  if (tid == 0) s_variance = rsqrtf(variance / ((float) K + epsilon));
+  // wait for s_variance in shared memory to be ready for all threads
+  __syncthreads();
+  if (idx < N * K) {
+    // x*y + z -> x'*g + b
+    y[idx] = __float2half(
+      __fmaf_rn(((value - s_mean) * s_variance), g, b)); 
+  }
 }
 
 // --------------------- PyTorch bindings for custom kernel -----------------------
@@ -124,6 +338,7 @@ if (((T2).size(0) != (T1).size(0)) || ((T2).size(1) != (T1).size(1))) { \
   throw std::runtime_error("Tensor size mismatch!");                    \
 }
 
+// fp32
 #define LANUCH_LAYER_NORM_F32_KERNEL(K)      \
 layer_norm_f32_kernel<(K)><<<grid, block>>>( \
   reinterpret_cast<float*>(x.data_ptr()),    \
@@ -206,7 +421,177 @@ void layer_norm_f32x4(torch::Tensor x, torch::Tensor y, float g, float b) {
   DISPATCH_LAYER_NORM_F32x4_KERNEL(N, K)
 }
 
+// fp16
+#define LANUCH_LAYER_NORM_F16F16_KERNEL(K)       \
+layer_norm_f16_f16_kernel<(K)><<<grid, block>>>( \
+  reinterpret_cast<half*>(x.data_ptr()),         \
+  reinterpret_cast<half*>(y.data_ptr()),         \
+  g, b, N, (K));  
+
+#define DISPATCH_LAYER_NORM_F16F16_KERNEL(N, K) \
+  dim3 block((K));                              \
+  dim3 grid((N));                               \     
+  switch ((K))                                  \
+  {                                             \
+  case 64:                                      \
+    LANUCH_LAYER_NORM_F16F16_KERNEL(64)         \
+    break;                                      \
+  case 128:                                     \
+    LANUCH_LAYER_NORM_F16F16_KERNEL(128)        \
+    break;                                      \
+  case 256:                                     \
+    LANUCH_LAYER_NORM_F16F16_KERNEL(256)        \
+    break;                                      \
+  case 512:                                     \
+    LANUCH_LAYER_NORM_F16F16_KERNEL(512)        \
+    break;                                      \
+  case 1024:                                    \
+    LANUCH_LAYER_NORM_F16F16_KERNEL(1024)       \
+    break;                                      \
+  default:                                      \
+    throw std::runtime_error(                   \
+      "only support K: 64/128/256/512/1024");   \
+    break;                                      \
+  } 
+
+#define LANUCH_LAYER_NORM_F16F32_KERNEL(K)       \
+layer_norm_f16_f32_kernel<(K)><<<grid, block>>>( \
+  reinterpret_cast<half*>(x.data_ptr()),         \
+  reinterpret_cast<half*>(y.data_ptr()),         \
+  g, b, N, (K));  
+
+#define DISPATCH_LAYER_NORM_F16F32_KERNEL(N, K) \
+  dim3 block((K));                              \
+  dim3 grid((N));                               \     
+  switch ((K))                                  \
+  {                                             \
+  case 64:                                      \
+    LANUCH_LAYER_NORM_F16F32_KERNEL(64)         \
+    break;                                      \
+  case 128:                                     \
+    LANUCH_LAYER_NORM_F16F32_KERNEL(128)        \
+    break;                                      \
+  case 256:                                     \
+    LANUCH_LAYER_NORM_F16F32_KERNEL(256)        \
+    break;                                      \
+  case 512:                                     \
+    LANUCH_LAYER_NORM_F16F32_KERNEL(512)        \
+    break;                                      \
+  case 1024:                                    \
+    LANUCH_LAYER_NORM_F16F32_KERNEL(1024)       \
+    break;                                      \
+  default:                                      \
+    throw std::runtime_error(                   \
+      "only support K: 64/128/256/512/1024");   \
+    break;                                      \
+  } 
+
+#define LANUCH_LAYER_NORM_F16x2F16_KERNEL(K)         \
+layer_norm_f16x2_f16_kernel<(K)/2><<<grid, block>>>( \
+  reinterpret_cast<half*>(x.data_ptr()),             \
+  reinterpret_cast<half*>(y.data_ptr()),             \
+  g, b, N, (K));  
+
+#define DISPATCH_LAYER_NORM_F16x2F16_KERNEL(N, K) \
+  dim3 block((K)/2);                              \
+  dim3 grid((N));                                 \     
+  switch ((K))                                    \
+  {                                               \
+  case 64:                                        \
+    LANUCH_LAYER_NORM_F16x2F16_KERNEL(64)         \
+    break;                                        \
+  case 128:                                       \
+    LANUCH_LAYER_NORM_F16x2F16_KERNEL(128)        \
+    break;                                        \
+  case 256:                                       \
+    LANUCH_LAYER_NORM_F16x2F16_KERNEL(256)        \
+    break;                                        \
+  case 512:                                       \
+    LANUCH_LAYER_NORM_F16x2F16_KERNEL(512)        \
+    break;                                        \
+  case 1024:                                      \
+    LANUCH_LAYER_NORM_F16x2F16_KERNEL(1024)       \
+    break;                                        \
+  default:                                        \
+    throw std::runtime_error(                     \
+      "only support K: 64/128/256/512/1024");     \
+    break;                                        \
+  } 
+
+#define LANUCH_LAYER_NORM_F16x8F16_KERNEL(K)         \
+layer_norm_f16x8_f16_kernel<(K)/8><<<grid, block>>>( \
+  reinterpret_cast<half*>(x.data_ptr()),             \
+  reinterpret_cast<half*>(y.data_ptr()),             \
+  g, b, N, (K));  
+
+#define DISPATCH_LAYER_NORM_F16x8F16_KERNEL(N, K) \
+  dim3 block((K)/8);                              \
+  dim3 grid((N));                                 \     
+  switch ((K))                                    \
+  {                                               \
+  case 64:                                        \
+    LANUCH_LAYER_NORM_F16x8F16_KERNEL(64)         \
+    break;                                        \
+  case 128:                                       \
+    LANUCH_LAYER_NORM_F16x8F16_KERNEL(128)        \
+    break;                                        \
+  case 256:                                       \
+    LANUCH_LAYER_NORM_F16x8F16_KERNEL(256)        \
+    break;                                        \
+  case 512:                                       \
+    LANUCH_LAYER_NORM_F16x8F16_KERNEL(512)        \
+    break;                                        \
+  case 1024:                                      \
+    LANUCH_LAYER_NORM_F16x8F16_KERNEL(1024)       \
+    break;                                        \
+  default:                                        \
+    throw std::runtime_error(                     \
+      "only support K: 64/128/256/512/1024");     \
+    break;                                        \
+  } 
+
+void layer_norm_f16_f16(torch::Tensor x, torch::Tensor y, float g, float b) {
+  CHECK_TORCH_TENSOR_DTYPE(x, torch::kHalf)       
+  CHECK_TORCH_TENSOR_DTYPE(y, torch::kHalf)
+  CHECK_TORCH_TENSOR_SHAPE(x, y)
+  const int N = x.size(0);
+  const int K = x.size(1);
+  DISPATCH_LAYER_NORM_F16F16_KERNEL(N, K)
+}
+
+void layer_norm_f16x2_f16(torch::Tensor x, torch::Tensor y, float g, float b) {
+  CHECK_TORCH_TENSOR_DTYPE(x, torch::kHalf)       
+  CHECK_TORCH_TENSOR_DTYPE(y, torch::kHalf)
+  CHECK_TORCH_TENSOR_SHAPE(x, y)
+  const int N = x.size(0);
+  const int K = x.size(1);
+  DISPATCH_LAYER_NORM_F16x2F16_KERNEL(N, K)
+}
+
+void layer_norm_f16x8_f16(torch::Tensor x, torch::Tensor y, float g, float b) {
+  CHECK_TORCH_TENSOR_DTYPE(x, torch::kHalf)       
+  CHECK_TORCH_TENSOR_DTYPE(y, torch::kHalf)
+  CHECK_TORCH_TENSOR_SHAPE(x, y)
+  const int N = x.size(0);
+  const int K = x.size(1);
+  DISPATCH_LAYER_NORM_F16x8F16_KERNEL(N, K)
+}
+
+void layer_norm_f16_f32(torch::Tensor x, torch::Tensor y, float g, float b) {
+  CHECK_TORCH_TENSOR_DTYPE(x, torch::kHalf)       
+  CHECK_TORCH_TENSOR_DTYPE(y, torch::kHalf)
+  CHECK_TORCH_TENSOR_SHAPE(x, y)
+  const int N = x.size(0);
+  const int K = x.size(1);
+  DISPATCH_LAYER_NORM_F16F32_KERNEL(N, K)
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   TORCH_BINDING_COMMON_EXTENSION(layer_norm_f32)
   TORCH_BINDING_COMMON_EXTENSION(layer_norm_f32x4)
+  TORCH_BINDING_COMMON_EXTENSION(layer_norm_f16_f16)
+  TORCH_BINDING_COMMON_EXTENSION(layer_norm_f16x2_f16)
+  TORCH_BINDING_COMMON_EXTENSION(layer_norm_f16x8_f16)
+  TORCH_BINDING_COMMON_EXTENSION(layer_norm_f16_f32)
 }
+
